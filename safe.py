5580af59ce0a54cb49b70126a178f47e9f1103f1
Lungu Constantin-Marian <111748828+CMarian02@users.noreply.github.com>
2023-03-12 22:04:43 +0200

    small changes: path, some locations, some lines +/-

--- safe.py
+++ safe.py
@@ -189,19 +189,19 @@ class Ui_MainWindow(object):
             if value == self.st_value and step == 0:
                 self.timer.stop()
                 print('done 3 secs')
-                playsound.playsound("C:/Users/gamer/OneDrive/Desktop/Projects/safe-cracking/sounds/safe_click.mp3")
+                playsound.playsound("C:/Users/gamer/Desktop/Project/safe-crack/sounds/safe_click.mp3")
                 step += 1
                 self.check_progress(value, self.nd_value)
             elif value == self.nd_value and step == 1:
                 self.timer.stop()
-                playsound.playsound("C:/Users/gamer/OneDrive/Desktop/Projects/safe-cracking/sounds/safe_clickl.mp3")
+                playsound.playsound("C:/Users/gamer/Desktop/Project/safe-crack/sounds/safe_clickl.mp3")
                 print('done 4 secs')
                 step += 1
                 self.check_progress(value, self.rd_value)
             elif value == self.rd_value and step == 2:
                 self.timer.stop()
                 print('done 5 secs')
-                playsound.playsound("C:/Users/gamer/OneDrive/Desktop/Projects/safe-cracking/sounds/broke_safe.mp3")
+                playsound.playsound("C:/Users/gamer/Desktop/Project/safe-crack/sounds/broke_safe.mp3")
                 step += 1
                 self.open_frame()
                 print('You Broke Safe! Good Job!')
